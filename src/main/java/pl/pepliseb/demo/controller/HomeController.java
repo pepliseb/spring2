e41ecf9cff384ed0400b78bf30eda2b2269bbf08
pepliseb <eman@onet.eu>
2018-02-24 11:55:56 +0100

    Initial commit

---
+++ src/main/java/pl/pepliseb/demo/controller/HomeController.java
@@ -0,0 +1,16 @@
+package pl.pepliseb.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "main/home";
+
+    }
+
+
+}
